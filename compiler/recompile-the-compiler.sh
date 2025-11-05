3bda8145496496431f6cbf49a870c884c760d048
Johannes Bader <olydis@users.noreply.github.com>
2025-11-04 22:51:57 -0500

    Update recompile-the-compiler.sh

--- compiler/recompile-the-compiler.sh
+++ compiler/recompile-the-compiler.sh
@@ -2,7 +2,9 @@
 
 set -euo pipefail
 
-# Uses compiler.dag to compile compiler.lamb into an updated compiler.dag
+# Uses compile_to_dag.dag to compile compiler.lamb into an updated compile_to_dag.dag and compile.dag 
 result=$(mktemp)
 cat "$(dirname "$0")/compiler.lamb" | "$(dirname "$0")/compile.sh" compile_to_dag > "$result"
-mv "$result" "$(dirname "$0")/compiler.dag"
+mv "$result" "$(dirname "$0")/compile_to_dag.dag"
+cat "$(dirname "$0")/compiler.lamb" | "$(dirname "$0")/compile.sh" compile > "$result"
+mv "$result" "$(dirname "$0")/compile.dag"
